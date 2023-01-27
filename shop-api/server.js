7eaea3a55cc679f500d3ea0287b3f0f2a4c5b47d
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2023-01-27 04:10:43 +0000

    mongo db connection establised

--- shop-api/server.js
+++ shop-api/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import { router } from "./src/router/router.js";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { mongoDbConnection } from "./src/database/connection.js";
 
 //server port
 const PORT = process.env.PORT || 8000;
@@ -7,8 +11,15 @@ const PORT = process.env.PORT || 8000;
 //creating a new express instance
 const app = express();
 
-//use router
+//mongodb connection
+mongoDbConnection
+
+
+//use routers
 app.use(router)
+
+
+
 //starting server on port 8000
 app.listen(PORT, async()=>{
   console.log(`Server listening on port ${PORT}`)
